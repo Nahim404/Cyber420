537d5bb9025fb6a95c2d0ceded2cdc6b03767818
Nahim404 <111033987+Nahim404@users.noreply.github.com>
2022-08-11 16:59:52 +0600

    Update Free2009.py

--- Free2009.py
+++ Free2009.py
@@ -96,11 +96,11 @@ def helpnote():
         
 
 
-basex1 = basex.encode('ascii')
-basex2 = base64.b64encode(basex1)
-basex3 = basex2.decode('ascii')
-base4 = (basex3).upper()
-basesplit = base4.replace('=', 'X').replace('A', '3').replace('B', '9').replace('C', '7').replace('D', '1').replace('E', '4').replace('M', '2').replace('L', '6').replace('F', '8').replace('N', 'E').replace('T', '8')
+
+
+
+
+
 
 
 class Main:
